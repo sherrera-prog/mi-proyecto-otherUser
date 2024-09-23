39adf7890769f4d7a66bf40272acb6e2a85d0395
Sergio H. <117312389+sherrera-prog@users.noreply.github.com>
2024-09-23 19:12:37 +0200

    Añadido archivo index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Llamar al archivo functions.php
+include ./src/functions.php
+
+    echo "Hola mundo, soy el archivo index.php - Git (trabajo individual)";
+
+
+//Llamar a la función saludar del archivo functions.php y mostrar el saludo.
+echo saludar();
+
+?>
